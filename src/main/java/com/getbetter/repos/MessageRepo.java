a1c99739684b66ebe84beb4bdc947ba65844bcc4
stoletnaedine <artur.islamgulov@gmail.com>
2019-03-30 22:18:35 +0300

    add forms (text, tag) and filter form (search by tag)

---
+++ src/main/java/com/getbetter/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.getbetter.repos;
+
+import com.getbetter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
