bbbca96162ae6b9f37f0abd88d509c9acf40d56a
stoletnaedine <artur.islamgulov@gmail.com>
2019-03-31 13:32:45 +0300

    add One to many connection with tables

--- src/main/java/com/getbetter/controller/MainController.java
+++ src/main/java/com/getbetter/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.getbetter.controller;
 
 import com.getbetter.domain.Message;
+import com.getbetter.domain.User;
 import com.getbetter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +34,13 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
