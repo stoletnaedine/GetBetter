453d5eb6a6264749332cb205b7209a74ff173af9
stoletnaedine <artur.islamgulov@gmail.com>
2019-03-31 21:03:17 +0300

    change to FreeMarker engine

--- src/main/java/com/getbetter/controller/MainController.java
+++ src/main/java/com/getbetter/controller/MainController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -25,10 +24,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        final Iterable<Message> messages = messageRepo.findAll();
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -37,31 +43,16 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag,
-            Map<String, Object> model
+            @RequestParam String tag, Map<String, Object> model
     ) {
         Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
-        final Iterable<Message> messages = messageRepo.findAll();
+        Iterable<Message> messages = messageRepo.findAll();
 
         model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
+        model.put("filter", "");
 
         return "main";
     }
